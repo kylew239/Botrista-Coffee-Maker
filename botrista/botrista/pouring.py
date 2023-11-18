e4cbe3a7ecb944275867c1abeff2ccde0be7b269
kyle <kylewang239@gmail.com>
2023-11-17 23:16:16 -0600

    Worked on pouring node: -Create tf listener to get starting location -Turned into action
    Tested using: `ros2 action send_goal /pour_action -f botrista_interfaces/action/PourAction "{spiral_radius: 0.05, num_points: 100, num_loops: 4.0, pour_frame: 'panda_hand'}"`
    
    TODO:
    -Add in orientations
    -Modify to use transform to spout of kettle rather than home position
    -quaternion to euler and reverse
    -Implement max speed
    -Test action on real robot
    
    Potential Issues:
    -path doesn't move smoothly - sometimes the robot will re-orient itself in the middle of the path so that it can finish the rest of the path

--- botrista/botrista/pouring.py
+++ botrista/botrista/pouring.py
@@ -4,6 +4,9 @@ import math
 import numpy as np
 import time
 from rclpy.callback_groups import ReentrantCallbackGroup
+from tf2_ros import TransformException
+from tf2_ros.transform_listener import TransformListener
+from tf2_ros.buffer import Buffer
 
 # Object Importing
 from moveit_wrapper.moveitapi import MoveItApi
@@ -12,6 +15,7 @@ from rcl_interfaces.msg import ParameterDescriptor
 from moveit_msgs.msg import RobotState
 from std_srvs.srv import Empty
 from rclpy.action import ActionServer
+from botrista_interfaces.action import PourAction
 
 
 class Pouring(Node):
@@ -21,6 +25,18 @@ class Pouring(Node):
         self.cb = ReentrantCallbackGroup()
         self.get_logger().warn("Pouring started")
 
+        # Creating Parameters
+        self.declare_parameter("pour_begin_offset", 0.05,
+                               ParameterDescriptor(
+                                   description="Z distance to move to before pouring"))
+        self.offset = self.get_parameter("pour_begin_offset")\
+                             .get_parameter_value().double_value
+
+        # Creating tf listener
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.tf_parent_frame = "panda_link0"
+
         # Moveit Wrapper Object
         self.moveit = MoveItApi(self,
                                 "panda_link0",
@@ -28,56 +44,63 @@ class Pouring(Node):
                                 "panda_manipulator",
                                 "joint_states")
 
-        # Creating Services
-        self.pour_client = self.create_service(Empty,
-                                               "pour_kettle",
-                                               self.pour_callback,
-                                               callback_group=self.cb)
-        self.execute = self.create_service(Empty,
-                                           "execute_traj",
-                                           self.execute_callback,
-                                           callback_group=self.cb)
-        
         # Creating action server
-        self._action_server = ActionServer(self, )
+        self._action_server = ActionServer(self,
+                                           PourAction,
+                                           'pour_action',
+                                           self.pour_callback,
+                                           callback_group=self.cb)
 
-        # TODO: get april tag home position and use that
-        self.home = Point(x=0.3069, y=0.0, z=0.487)
-        self.homePose = Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
-
-    async def pour_callback(self, request, response):
+    async def pour_callback(self, goal_handle):
         # TODO: Fill in
-        self.get_logger().warn("started cb")
-        waypoints = get_spiral_waypoints(self.home,
-                                         1000,
-                                         0.1,
-                                         10.0,
-                                         endStart=False)
-        result = await self.moveit.create_cartesian_path(waypoints)
-        self.get_logger().warn("finished await")
-        self.path = result.trajectory
-        return response
+        result = PourAction.Result()
+        feedback = PourAction.Feedback()
+        req = goal_handle.request
 
-    async def execute_callback(self, request, response):
-        # Temporary
-        self.moveit.execute_trajectory(self.path)
+        # Attempt to get point from frame
+        try:
+            tf = self.tf_buffer.lookup_transform(
+                self.tf_parent_frame,
+                req.pour_frame,
+                rclpy.time.Time())
+            startVec = tf.transform.translation
+            startPoint = Point(x=startVec.x, y=startVec.y, z=startVec.z)
+            startOre = tf.transform.rotation
+        except TransformException:
+            feedback.stage = f"Failed to get transform to {req.pour_frame}"
+            goal_handle.publish_feedback(feedback)
+            result.status = False
+            return result
 
-    async def pour_callback(self, request, response):
-        # TODO: Fill in
-        waypoints = get_spiral_waypoints(self.home,
-                                         100,
-                                         0.1,
-                                         4.0,
-                                         flipStart=False)
-        result = await self.moveit.create_cartesian_path(waypoints)
-        print("finished await")
-        self.path = result.trajectory
-        return response
+        # Calculating path
+        feedback.stage = "Calculating path"
+        goal_handle.publish_feedback(feedback)
+        waypoints = get_spiral_waypoints(startPoint,
+                                         startOre,
+                                         req.num_points,
+                                         req.spiral_radius,
+                                         req.num_loops,
+                                         req.start_outside)
+        standoff_point = startPoint
+        standoff_point.z += self.offset
+        standoffPose = Pose(position=standoff_point,
+                             orientation=startOre)
+        waypoints.append(standoffPose)
+        waypoints.insert(0, standoffPose)
 
-    async def execute_callback(self, request, response):
-        # Temporary
-        self.moveit.execute_trajectory(self.path)
-        return response
+        # Planning
+        feedback.stage = "Planning path"
+        goal_handle.publish_feedback(feedback)
+        planned_traj = await self.moveit.create_cartesian_path(waypoints)
+
+        # Executing path
+        feedback.stage = "Executing path"
+        goal_handle.publish_feedback(feedback)
+        self.moveit.execute_trajectory(planned_traj.trajectory)
+
+        goal_handle.succeed()
+        result.status = True
+        return result
 
 
 def main(args=None):
@@ -88,6 +111,7 @@ def main(args=None):
 
 
 def get_spiral_waypoints(startPoint: Point,
+                         startOre: Quaternion,
                          numPoints: int,
                          maxRadius: float,
                          loops: float,
@@ -97,6 +121,7 @@ def get_spiral_waypoints(startPoint: Point,
 
     Arguments:
         startPoint (geometry_msgs/Point) -- Starting point
+        startOre (geometry_msgs/Quaternion) -- Starting Orientation
         numPoints (int) -- number of points used to build the path
         maxRadius (float) -- distance from end of spiral to origin in cm
         loops (float) -- number of loops for the spiral to go through
@@ -124,10 +149,7 @@ def get_spiral_waypoints(startPoint: Point,
         poseList.append(Pose(position=Point(x=x,
                                             y=y,
                                             z=startPoint.z),
-                             orientation=Quaternion(x=1.0,
-                                                    y=0.,
-                                                    z=0.,
-                                                    w=0.)))
+                             orientation=startOre))
 
         count += 1
 
