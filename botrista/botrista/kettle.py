a8b4a31ddca009616198d51864989063eb63b52b
Carter DiOrio <carterdiorio2024@u.northwestern.edu>
2023-11-27 15:09:46 -0600

    fixed duplicated grab

--- botrista/botrista/kettle.py
+++ botrista/botrista/kettle.py
@@ -68,53 +68,6 @@ class Kettle(Node):
 
         return response
 
-        self.buffer = Buffer()
-        self.listener = TransformListener(self.buffer, self)
-        self.moveit_api = MoveItApi(
-            self, "panda_link0", "panda_hand_tcp", "panda_manipulator", "/franka/joint_states")
-        self.grasp_planner = GraspPlanner(
-            self.moveit_api, "panda_gripper/grasp")
-
-        self.srv = self.create_service(
-            Empty, "grab", self.grab, callback_group=ReentrantCallbackGroup())
-
-    async def grab(self, request, response):
-        tf = self.buffer.lookup_transform(
-            "panda_link0", "filtered_kettle_tag", Time())
-
-        approach_pose = Pose(
-            position=Point(x=0.006, y=0.05, z=0.20),
-            orientation=Quaternion(x=0.88, y=-0.035, z=0.47, w=0.01)
-        )
-        approach_pose = tf2_geometry_msgs.do_transform_pose(approach_pose, tf)
-
-        grasp_pose = Pose(
-            position=Point(x=0.097, y=0.043, z=0.15),
-            orientation=Quaternion(x=0.88, y=-0.035, z=0.47, w=0.01)
-        )
-        grasp_pose = tf2_geometry_msgs.do_transform_pose(grasp_pose, tf)
-
-        retreat_pose = Pose(
-            position=Point(x=0.097, y=0.043, z=0.25),
-            orientation=Quaternion(x=0.88, y=-0.035, z=0.47, w=0.01)
-        )
-        retreat_pose = tf2_geometry_msgs.do_transform_pose(retreat_pose, tf)
-
-        grasp_plan = GraspPlan(
-            approach_pose=approach_pose,
-            grasp_pose=grasp_pose,
-            grasp_command=Grasp.Goal(
-                width=0.02,
-                force=50.0,
-                speed=0.05,
-            ),
-            retreat_pose=retreat_pose,
-        )
-
-        await self.grasp_planner.execute_grasp_plan(grasp_plan)
-
-        return response
-
 
 def kettle_entry(args=None):
     rclpy.init(args=args)
