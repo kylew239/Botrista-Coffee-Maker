be2a6e9cc6e79c0035c60fcd3e27a4655bf40f9c
Stephen <stephencferro@gmail.com>
2023-12-01 06:52:29 -0600

    Added action clients to run_botrista

--- botrista/botrista/run_botrista.py
+++ botrista/botrista/run_botrista.py
@@ -11,18 +11,19 @@ from franka_msgs.action import Grasp
 from rclpy.time import Time
 from time import sleep
 from franka_msgs.msg import GraspEpsilon
-from botrista_interfaces.action import EmptyAction, GraspProcess
+from botrista_interfaces.action import EmptyAction
+from botrista_interfaces.action import PourAction
 from rclpy.action import ActionServer, ActionClient
 
 
 class Botrista(Node):
     """
     Description:
-        Waits to detect a cup placed in the detection area, then runs following routine:
+        Waits to detect an empty cup placed in the detection area, then runs following routine:
             1. Turns on Kettle (action)
             2. Pick up Filter from filter stand (action)
             3. Place Filter on Pot (action)
-            4. Scoop grounds (action)
+            4. Scoop grounds (scoop action)
             5. Wait for boiling
             6. Pick up Kettle (pick_kettle action)
             7. Pour water from kettle (pour_action action)
@@ -49,7 +50,13 @@ class Botrista(Node):
         # start action client for place_pot action
         self.action_client_place_pot = ActionClient(self, EmptyAction, 'place_pot')
 
-        # start action client for place_pot action
+        # start action client for pour_action action
+        self.action_client_pour_action = ActionClient(self, PourAction, 'pour_action')
+        
+        # start action client for scoop action
+        self.action_client_scoop = ActionClient(self, EmptyAction, 'scoop')
+
+        # start action client for pour_action action
         self.action_client_pour_action = ActionClient(self, PourAction, 'pour_action')
 
 
