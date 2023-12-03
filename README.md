3607b640cb19443085c8726d4dae3e24844040ba
Stephen <stephencferro@gmail.com>
2023-12-03 15:19:11 -0600

    Began filling out README. Description and Authors completed.

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-[kettle-7] [WARN] [1701346059.323087341] [kettle]: Transform in kettle geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1701346053, nanosec=461528503), 
-frame_id='panda_link0'), child_frame_id='handle', 
-transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(
-x=0.413753037199253, y=0.4511147477411885, z=0.17383633681074095), 
-rotation=geometry_msgs.msg.Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)))
+# ME495 Final Project - Group 1 - Botrista
 
-[handle_detector-9] [WARN] [1701346059.346060879] [handle_detector]: Transform geometry_msgs.msg.Pose(
-    position=geometry_msgs.msg.Point(x=0.42643299118522027, y=0.45100312174703266, z=0.17380417191545317), 
-    orientation=geometry_msgs.msg.Quaternion(x=0.7039161086168184, y=-0.7102778497167178, z=0.0013348671765198322, w=0.00238879910666776)) builtin_interfaces.msg.Time
-    (sec=1701346059, nanosec=345542800)
+Authors: Stephen Ferro, Anuj Natraj, Carter DiOrio, Kyle Wang, and Jihai Zhao
 
-[apriltag_node-4] [WARN] [1701346811.799343599] [apriltag]: [image_transport] Topics '/image_rect/compressed' and '/camera_info' do not appear to be synchronized. In the last 10s:
-[apriltag_node-4] 	Image messages received:      1
-[apriltag_node-4] 	CameraInfo messages received: 6
-[apriltag_node-4] 	Synchronized pairs:           1
+Description: A collection of ROS2 packages to drive the Emika Franka robot arm to brew a cup of pour over coffee. 
+
+Usage instructions:
+1. 
+
+Configuration instructions:
+1. 
--- botrista/botrista/coffee_grounds.py
+++ botrista/botrista/coffee_grounds.py
@@ -1,3 +1,13 @@
+"""
+Manages the scoop action, which picks up the scoop, scoops coffee, move to the coffee pot, and dumps the coffee
+
+ACTION CLIENTS:
+  + grasp_process (GraspProcess) - A seriers of movements and gripper commands for the robot to complete a grasp action
+
+ACTION SERVERS:
+  + scoop (EmptyAction) - The scooping routine
+"""
+
 from rclpy.node import Node
 import rclpy
 import numpy as np
@@ -16,19 +26,13 @@ from tf2_ros import TransformListener, Buffer
 from botrista_interfaces.action import EmptyAction
 from botrista_interfaces.action import GraspProcess
 from rclpy.time import Time
-from sensor_msgs.msg import Image, CameraInfo
-import cv_bridge
-from image_geometry.cameramodels import PinholeCameraModel
-import pyrealsense2
 from moveit_wrapper.grasp_planner import GraspPlanner, GraspPlan
-
 
 class CoffeeGrounds(Node):
     """
     Measures coffee depth, scoops coffee, and dumps coffee in coffee maker.
     Also dumps used coffee grounds from filter.
     """
-
     def __init__(self):
         """
         Description:
@@ -36,7 +40,7 @@ class CoffeeGrounds(Node):
         """
         super().__init__('coffee_grounds')
 
-        # define observation poses (relative to april tag)
+        # Define observation poses (relative to april tag)
         self.scoop_offset_pose_observe = Pose(
             position=Point(
                 x=-0.1, y=0.02, z=0.4),
@@ -44,7 +48,7 @@ class CoffeeGrounds(Node):
                 x=.707388, y=-0.706825, z=0.0005629, w=0.0005633)
         )
 
-        # define refinement, approach, grasp, and retreat poses (relative to handle)
+        # Define refinement, approach, grasp, and retreat poses (relative to handle)
         self.scoop_offset_pose_refine = Pose(
             position=Point(
                 x=0.0, y=0.0, z=-0.20),
@@ -69,7 +73,7 @@ class CoffeeGrounds(Node):
                 x=0.5, y=0.5, z=0.5, w=0.5)
         )
 
-        # define scooping poses (relative to coffee grounds april tag)
+        # Define scooping poses (relative to coffee grounds april tag)
         self.scoop_pose_measure = Pose(
             position=Point(
                 x=-0.15, y=0.04, z=0.2),
@@ -91,7 +95,7 @@ class CoffeeGrounds(Node):
             orientation=Quaternion(
                 x=-0.7071068, y=-0.0, z=0.0, w=0.7071068))
 
-        # define dumping poses (relative to pot top)
+        # Define dumping poses (relative to pot top)
         self.dump_pose_approach = Pose(
             position=Point(
                 x=0.0, y=0.0, z=0.2),
@@ -100,46 +104,21 @@ class CoffeeGrounds(Node):
         # Initialize actual pickup positions
         self.scoop_actual_pickup_pose = Pose()
 
-        # define grasp commands
+        # Define grasp commands
         self.grasp_command_scoop = Grasp.Goal(
             width=0.02, force=50.0, speed=0.05)
         self.grasp_command_open = Grasp.Goal(
             width=0.02, force=50.0, speed=0.05)
 
-        # start action server for scooping routine
+        # Start action server for scooping routine
         self.scoop_action_server = ActionServer(
             self,
             EmptyAction,
             'scoop',
             self.scoop_coffee_grounds)
 
-        # start action client for grasp process action
+        # Start action client for grasp process action
         self.grasp_process = ActionClient(self, GraspProcess, 'grasp_process')
-
-        # create subscription to camera topic
-        # self.image_subscription = self.create_subscription(
-        #     Image, "/camera/d405/color/image_rect_raw", self.image_callback, qos_profile=10)
-
-        # subscription to the depth image
-        self.depth_image_subscription = self.create_subscription(
-            Image, "/camera/d405/aligned_depth_to_color/image_raw", self.depth_image_callback, qos_profile=10
-        )
-
-        self.camera_info_subscription = self.create_subscription(
-            CameraInfo, "/camera/d405/aligned_depth_to_color/camera_info", self.camera_info_callback, qos_profile=10)
-
-        # published the masked depth image
-        self.depth_publisher = self.create_publisher(
-            Image, "/depth_mask", qos_profile=10
-        )
-
-        self.cv_bridge = cv_bridge.CvBridge()
-
-        self.camera_model = PinholeCameraModel()
-
-        # camera intrinsics
-        self.depth_image = None
-        self.intrinsics = None
 
         # Create tf listener
         self.tf_buffer = Buffer()
@@ -162,24 +141,24 @@ class CoffeeGrounds(Node):
         Description:
             Action callback for the scooping routine, scoops coffee and dumps it in the filter
         """
-        # self.measure_coffee_height()
         await self.grab_scoop()
-        # self.scoop_grounds()
+        await self.scoop_grounds()
         await self.move_to_pot()
 
-        # go home
+        # Go home
         await self.moveit_api.go_home()
         await self.return_scoop()
 
         goal_handle.succeed()
         return EmptyAction.Result()
 
+
     async def move_to_pot(self):
         """
         Moves the scoop to the pot to dump grounds.
         """
 
-        # create horizontal path constraint
+        # Create horizontal path constraint
         current_pose = await self.moveit_api.get_end_effector_pose()
         path_constraints = self.moveit_api.create_path_constraints(
             current_pose.pose.orientation,
@@ -188,7 +167,7 @@ class CoffeeGrounds(Node):
             z_tol=np.pi / 8,
         )
 
-        # get the pot top pose
+        # Get the pot top pose
         pot_tf = await self.tf_buffer.lookup_transform_async(
             "panda_link0", "pot_top", Time())
 
@@ -275,6 +254,7 @@ class CoffeeGrounds(Node):
             execute=True,
         )
 
+
     async def grab_scoop(self):
         """
         Description:
@@ -309,7 +289,12 @@ class CoffeeGrounds(Node):
         res = await goal.get_result_async()
         self.scoop_actual_pickup_pose = res.result.actual_grasp_pose
 
+
     async def scoop_grounds(self):
+        """
+        Description:
+            Function to scoop coffee grounds from the coffee pot
+        """
         tf = self.buffer.lookup_transform(
             "filtered_grounds_tag", "panda_link0", Time())
 
@@ -341,7 +326,12 @@ class CoffeeGrounds(Node):
             use_jc=True
         )
 
+
     async def return_scoop(self):
+        """
+        Description:
+            Function to place the coffee scoop on the scoop tower
+        """
         approach_pose = Pose(
             position=Point(x=0.0, y=-0.20, z=0.0),
             orientation=Quaternion()
@@ -376,79 +366,6 @@ class CoffeeGrounds(Node):
         )
 
         await self.grasp_planner.execute_grasp_plan(grasp_plan)
-
-    async def flip_shake_filter(self):
-        tf = self.buffer.lookup_transform(
-            "panda_link0", "filtered_TRASH_tag", Time())
-
-        approach_pose = Pose(
-            position=self.filter_dump_pos_approach,
-            orientation=self.dump_orientation_upright
-        )
-        approach_pose = tf2_geometry_msgs.do_transform_pose(approach_pose, tf)
-
-        grasp_pose = Pose(
-            position=self.filter_dump_pos,
-            orientation=self.dump_orientation_flipped
-        )
-        grasp_pose = tf2_geometry_msgs.do_transform_pose(grasp_pose, tf)
-
-        retreat_pose = Pose(
-            position=self.filter_dump_pos_retreat,
-            orientation=self.dump_orientation_upright
-        )
-        retreat_pose = tf2_geometry_msgs.do_transform_pose(retreat_pose, tf)
-
-        grasp_plan = GraspPlan(
-            approach_pose=approach_pose,
-            grasp_pose=grasp_pose,
-            grasp_command=self.grasp_command_filter,
-            retreat_pose=retreat_pose,
-        )
-
-        await self.grasp_planner.execute_grasp_plan(grasp_plan)
-
-    def camera_info_callback(self, camera_info):
-        self.intrinsics = pyrealsense2.intrinsics()
-        self.intrinsics.width = camera_info.width
-        self.intrinsics.height = camera_info.height
-        self.intrinsics.ppx = camera_info.k[2]
-        self.intrinsics.ppy = camera_info.k[5]
-        self.intrinsics.fx = camera_info.k[0]
-        self.intrinsics.fy = camera_info.k[4]
-        self.intrinsics.model = pyrealsense2.distortion.none
-        self.intrinsics.coeffs = [i for i in camera_info.d]
-
-    def depth_image_callback(self, image):
-        self.depth_image = self.cv_bridge.imgmsg_to_cv2(
-            image, desired_encoding="passthrough")
-
-    def measure_coffee_height(self):
-        self.move_to_measure_pose()
-
-        # get the 3d point
-        point = pyrealsense2.rs2_deproject_pixel_to_point(
-            self.intrinsics, [cX, cY], depth)
-        point = np.array(point)
-        point /= 1000.0  # mm to m
-
-    def move_to_measure_pose(self):
-        """
-        Description: Move robot to specifcied position and orientation to measure coffee ground height
-        """
-        tf = self.buffer.lookup_transform(
-            "panda_link0", "filtered_coffee_grounds", Time())
-
-        measure_pose = tf2_geometry_msgs.do_transform_pose(
-            self.scoop_pose_measure, tf)
-
-        fut = self.moveit_api.plan(
-            point=measure_pose.position,
-            orientation=measure_pose.orientation,
-            execute=True,
-            use_jc=True
-        )
-        return fut
 
 
 def coffee_grounds_entry(args=None):
